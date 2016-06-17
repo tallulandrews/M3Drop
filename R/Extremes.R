110804257f6dbf247c9788d98e71e7c8af574948
Tallulah Andrews <ta6@sanger.ac.uk>
2016-06-17 11:33:46 +0100

    Fix the fix

--- R/Extremes.R
+++ R/Extremes.R
@@ -27,6 +27,8 @@ bg__test_DE_K_equiv <- function(expr_mat, fit=NA) {
 		fit = bg__fit_MM(gene_info$p, gene_info$s);
 	}
 	p_obs = gene_info$p;
+	always_detected = p_obs==0
+	p_obs[p_obs==0] = min(p_obs[p_obs > 0])/2 # Here so that K_equiv for p_obs==0 will be 0 but K_equiv_err will not throw errors for such genes.
 	N = length(expr_mat[1,]);
 	p_err = gene_info$p_stderr;
 	S_mean = gene_info$s
@@ -34,8 +36,8 @@ bg__test_DE_K_equiv <- function(expr_mat, fit=NA) {
 	K_err = fit$Kerr;
 	K_obs = fit$K
 	K_equiv = p_obs*S_mean/(1-p_obs);
-	p_obs[p_obs==0] = min(p_obs[p_obs > 0])/2 # Here so that K_equiv for p_obs==0 will be 0 but K_equiv_err will not throw errors for such genes.
 	K_equiv_err = abs(K_equiv)*sqrt((S_err/S_mean)^2 + (p_err/p_obs)^2)
+
 
 	K_equiv_log = log(K_equiv)
 	thing = K_equiv-K_equiv_err; thing[thing <= 0 ] = 10^-100
@@ -48,6 +50,7 @@ bg__test_DE_K_equiv <- function(expr_mat, fit=NA) {
 		
 	Z = (K_equiv_log - K_obs_log)/sqrt(K_equiv_err_log^2+K_err_log^2); # high = shifted right, low = shifted left
 	pval = pnorm(Z, lower.tail=F)
+	pval[always_detected] = 1;
 	effect_size = K_equiv/fit$K;
 	return(list(pval = pval, fold_change = effect_size))
 }
