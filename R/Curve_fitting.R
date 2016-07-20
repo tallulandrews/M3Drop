e5a3089d85646d8ccb4a3996aa0adf24dee375d0
Tallulah Andrews <ta6@sanger.ac.uk>
2016-07-20 16:47:10 +0100

    Change = to <-

--- R/Curve_fitting.R
+++ R/Curve_fitting.R
@@ -4,17 +4,17 @@ bg__fit_MM <- function (p,s) {
 	}
 	#require("bbmle")
 	LL <- function(krt,sigma) {
-		R = p-(1-(s/((krt+s))))
-		R = suppressWarnings(dnorm(R,0,sigma,log=TRUE))
+		R <- p-(1-(s/((krt+s))))
+		R <- suppressWarnings(dnorm(R,0,sigma,log=TRUE))
 		-sum(R)
 	}
-	fit = mle2(LL,start=list(krt=3, sigma=0.25))
-	thing = summary(fit)
-	krt = fit@coef[1]
-	res_err = fit@coef[2];
-	Kerr = max(fit@coef[2],attributes(summary(fit))$coef[1,2]);
-	predicted = 1-(s/(krt+s))
-	residuals = p-predicted
+	fit <- mle2(LL,start=list(krt=3, sigma=0.25))
+	thing <- summary(fit)
+	krt <- fit@coef[1]
+	res_err <- fit@coef[2];
+	Kerr <- max(fit@coef[2],attributes(summary(fit))$coef[1,2]);
+	predicted <- 1-(s/(krt+s))
+	residuals <- p-predicted
 	return(list(K=krt,Kerr=Kerr,fitted_err = res_err,predictions=predicted, model=c("MMenten",paste("K =",round(krt,digits=3))),SSr=round(sum((residuals)^2)),SAr=round(sum(abs(residuals)))))
 }
 
@@ -24,47 +24,47 @@ hidden__fit_MM_lognormal<-function(p,s){
 		stop(print("Error: p and s not same length. Cannot fit Michaelis-Menten."))
 	}
 	#require("bbmle")
-	p_c = p[p<1 & p>0]
-	s_c = s[p<1 & p>0]
+	p_c <- p[p<1 & p>0]
+	s_c <- s[p<1 & p>0]
 	LL <- function(krt,sigma) {
 		if (krt >0) {
-			obs_Ks = p_c/(1-p_c)*s_c
-			R = log(obs_Ks)-log(krt);
-			Qs = quantile(R,prob=c(0.25,0.5,0.75))
-			IQR = Qs[2]-Qs[1];
+			obs_Ks <- p_c/(1-p_c)*s_c
+			R <- log(obs_Ks)-log(krt);
+			Qs <- quantile(R,prob=c(0.25,0.5,0.75))
+			IQR <- Qs[2]-Qs[1];
 
-			thing = densCols(p_c, log(s_c)/log(10), colramp = colorRampPalette(c("black","white")));
-			dens = colSums(col2rgb(thing))
-			thresh = quantile(dens, prob=0.05);
+			thing <- densCols(p_c, log(s_c)/log(10), colramp = colorRampPalette(c("black","white")));
+			dens <- colSums(col2rgb(thing))
+			thresh <- quantile(dens, prob=0.05);
 
-			R = R[dens > thresh]
-			R = suppressWarnings(dnorm(R,0,sigma,log=TRUE))
+			R <- R[dens > thresh]
+			R <- suppressWarnings(dnorm(R,0,sigma,log=TRUE))
 			-sum(R)
 		} else {
 			10^100
 		}
 	}
-	fit = mle2(LL,start=list(krt=6, sigma=0.25))
-	thing = summary(fit)
-	krt = fit@coef[1]
-	res_err = fit@coef[2];
-	Kerr = max(fit@coef[2],attributes(summary(fit))$coef[1,2]);
-	predicted = 1-(s/(krt+s))
-	residuals = p-predicted
+	fit <- mle2(LL,start=list(krt=6, sigma=0.25))
+	thing <- summary(fit)
+	krt <- fit@coef[1]
+	res_err <- fit@coef[2];
+	Kerr <- max(fit@coef[2],attributes(summary(fit))$coef[1,2]);
+	predicted <- 1-(s/(krt+s))
+	residuals <- p-predicted
 	return(list(K=krt,Kerr=Kerr,fitted_err = res_err,predictions=predicted, model=c("MMenten",paste("K =",round(krt,digits=3))),SSr=round(sum((residuals)^2)),SAr=round(sum(abs(residuals)))))
 }
 
 hidden__fit_MM_logistic <- function(p,s){
-	s_nozero = s[s>0];
-	p_nozero = p[s>0];
-	fit = glm(p_nozero ~ offset(-1*log(s_nozero)), family="binomial")
-	Kerr = summary(fit)$coeff[1,2];
-	Kcoeff = summary(fit)$coeff[1,1];
-	krt=exp(Kcoeff)
-	Kerr = Kerr*krt
-	predicted = rep(0, times=length(s));
-	predicted[s>0] = fitted(fit)
-	residuals = p-predicted
+	s_nozero <- s[s>0];
+	p_nozero <- p[s>0];
+	fit <- glm(p_nozero ~ offset(-1*log(s_nozero)), family="binomial")
+	Kerr <- summary(fit)$coeff[1,2];
+	Kcoeff <- summary(fit)$coeff[1,1];
+	krt <- exp(Kcoeff)
+	Kerr <- Kerr*krt
+	predicted <- rep(0, times=length(s));
+	predicted[s>0] <- fitted(fit)
+	residuals <- p-predicted
 	return(list(K=krt,Kerr=Kerr,predictions=predicted, model=c("MMenten",paste("K =",round(krt,digits=3))),SSr=round(sum((residuals)^2)),SAr=round(sum(abs(residuals)))))
 }
 
@@ -72,13 +72,13 @@ bg__fit_logistic <- function(p,s) {
 	if (length(p) != length(s)) {
 		stop(print("Error: p and s not same length. Cannot fit Logistic Regression."))
 	}
-	s_nozero = s[s>0];
-	p_nozero = p[s>0];
-        logistic = suppressWarnings(glm(p_nozero~log(s_nozero),family="binomial")) #warns that is not binary data
-        predlog = fitted(logistic)
-	fullpredictions = rep(0, times=length(s));
-	fullpredictions[s>0] = predlog
-	res = fullpredictions-p;
+	s_nozero <- s[s>0];
+	p_nozero <- p[s>0];
+        logistic <- suppressWarnings(glm(p_nozero~log(s_nozero),family="binomial")) #warns that is not binary data
+        predlog <- fitted(logistic)
+	fullpredictions <- rep(0, times=length(s));
+	fullpredictions[s>0] <- predlog
+	res <- fullpredictions-p;
 	return(list(predictions=fullpredictions, B0 = logistic$coeff[1], B1=logistic$coeff[2] ,model=c( "Logistic", paste("Intercept =",round(logistic$coeff[1],digits=3)),paste("Coeff =",round(logistic$coeff[2],digits=3))),SSr=round(sum(res^2)),SAr=round(sum(abs(res)))));
 }
 
@@ -86,25 +86,25 @@ bg__fit_ZIFA <- function(p,s) {
 	if (length(p) != length(s)) {
 		stop(print("Error: p and s not same length. Cannot fit double exponential."))
 	}
-	p_nozero = p; p_nozero[p == 0] = min(p[p>0])/10
-	fit = lm(log(p_nozero)~-1+s^2)
-	lambda = -fit$coeff[1]
-	res_err = attributes(summary(fit))$coef[2,1]
-	Lerr = summary(fit)$coeff[1,2]
-	predicted = exp(-lambda*s*s)
-	residuals = p-predicted
+	p_nozero <- p; p_nozero[p == 0] = min(p[p>0])/10
+	fit <- lm(log(p_nozero)~-1+s^2)
+	lambda <- -fit$coeff[1]
+	res_err <- attributes(summary(fit))$coef[2,1]
+	Lerr <- summary(fit)$coeff[1,2]
+	predicted <- exp(-lambda*s*s)
+	residuals <- p-predicted
 	return(list(lambda=lambda,Lerr=Lerr,fitted_err = res_err,predictions=predicted, model=c("p ~ e^(-lambda*S^2)",paste("lambda =",signif(lambda,digits=2))),SSr=round(sum((residuals)^2)),SAr=round(sum(abs(residuals)))))
 }
 
 M3Drop_Dropout_Models <- function(expr_mat, xlim=NA, suppress.plot=FALSE) {
-	BasePlot = bg__dropout_plot_base(expr_mat, xlim = xlim, suppress.plot=suppress.plot);
-	MM = bg__fit_MM(BasePlot$p, BasePlot$s);
-	SCDE = bg__fit_logistic(BasePlot$p, BasePlot$s);
-	ZIFA = bg__fit_ZIFA(BasePlot$p, BasePlot$s);
+	BasePlot <- bg__dropout_plot_base(expr_mat, xlim = xlim, suppress.plot=suppress.plot);
+	MM <- bg__fit_MM(BasePlot$p, BasePlot$s);
+	SCDE <- bg__fit_logistic(BasePlot$p, BasePlot$s);
+	ZIFA <- bg__fit_ZIFA(BasePlot$p, BasePlot$s);
 	if (!suppress.plot) {
-	  	sizeloc = bg__add_model_to_plot(MM, BasePlot, lty=1, lwd=2.5, col="black",legend_loc = "topright");
-		sizeloc = bg__add_model_to_plot(SCDE, BasePlot, lty=2, lwd=2.5, col="magenta3",legend_loc = c(sizeloc$rect$left+sizeloc$rect$w,sizeloc$rect$top-sizeloc$rect$h-0.05));
-		sizeloc = bg__add_model_to_plot(ZIFA, BasePlot, lty=3, lwd=2.5, col="red",legend_loc = c(sizeloc$rect$left+sizeloc$rect$w,sizeloc$rect$top-sizeloc$rect$h-0.05));
+	  	sizeloc <- bg__add_model_to_plot(MM, BasePlot, lty=1, lwd=2.5, col="black",legend_loc = "topright");
+		sizeloc <- bg__add_model_to_plot(SCDE, BasePlot, lty=2, lwd=2.5, col="magenta3",legend_loc = c(sizeloc$rect$left+sizeloc$rect$w,sizeloc$rect$top-sizeloc$rect$h-0.05));
+		sizeloc <- bg__add_model_to_plot(ZIFA, BasePlot, lty=3, lwd=2.5, col="red",legend_loc = c(sizeloc$rect$left+sizeloc$rect$w,sizeloc$rect$top-sizeloc$rect$h-0.05));
 	}
 	invisible(list(MMFit = MM, LogiFit = SCDE, ExpoFit = ZIFA));
 }
