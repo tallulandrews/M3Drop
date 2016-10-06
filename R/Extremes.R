289c0c213c35536a58162d0ebd22c2e3a6a0f685
Tallulah Andrews <ta6@sanger.ac.uk>
2016-10-06 10:29:51 +0100

    Homogenize argument order

--- R/Extremes.R
+++ R/Extremes.R
@@ -126,7 +126,7 @@ hidden__test_DE_S_equiv <- function (expr_mat, fit=NA, method="propagate") {
 	return(list(pval = pval, effect = effect_size))
 }
 
-bg__get_extreme_residuals <- function (expr_mat, fit=NA, v_threshold=c(0.05,0.95), percent = NA, fdr_threshold = 0.1, direction="right", suppress.plot = FALSE) {
+bg__get_extreme_residuals <- function (expr_mat, fit=NA, fdr_threshold = 0.1, percent = NA, v_threshold=c(0.05,0.95), direction="right", suppress.plot = FALSE) {
 	gene_info = bg__calc_variables(expr_mat);
 	if (is.na(fit)[1]) {
 		fit <- bg__fit_MM(gene_info$p, gene_info$s);
