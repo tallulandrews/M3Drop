40b6649c59a6a1bd8e14b529b57db9085578ac0d
Tallulah Andrews <ta6@sanger.ac.uk>
2017-10-04 15:22:11 +0100

    Optimizing NBumi for spare matrices

--- R/NB_UMI.R
+++ R/NB_UMI.R
@@ -3,42 +3,48 @@ hidden_calc_vals <- function(counts) {
         if (sum(counts < 0) >0) {stop("Expression matrix contains negative values! Please provide raw UMI counts!")}
         if (sum(!is.integer(counts)) >0) {stop("Expression matrix is not integers! Please provide a matrix (not data.frame) raw UMI counts!")}
 
-        tjs = rowSums(counts, na.rm=T) # Total molecules/gene
+        tjs <- rowSums(counts, na.rm=T) # Total molecules/gene
 	if (sum(tjs <= 0) > 0) {stop("Error: all genes must have at least one detected molecule.")}
-        tis = colSums(counts, na.rm=T) # Total molecules/cell
+        tis <- colSums(counts, na.rm=T) # Total molecules/cell
 	if (sum(tis <= 0) > 0) {stop("Error: all cells must have at least one detected molecule.")}
-        djs = rowSums(counts == 0, na.rm=T) # Observed Dropouts per gene
-        dis = colSums(counts == 0, na.rm=T) # Observed Dropouts per cell
-        nc = length(counts[1,]) # Number of cells
-        ng = length(counts[,1]) # Number of genes
-        total = sum(tis, na.rm=T) # Total molecules sampled
+        djs <- nrow(counts)-rowSums(counts > 0, na.rm=T) # Observed Dropouts per gene
+        dis <- ncol(counts)-colSums(counts > 0, na.rm=T) # Observed Dropouts per cell
+        nc <- length(counts[1,]) # Number of cells
+        ng <- length(counts[,1]) # Number of genes
+        total <- sum(tis, na.rm=T) # Total molecules sampled
         return(list(tis=tis, tjs=tjs, dis=dis, djs=djs, total=total,nc=nc,ng=ng));
 }
 
 NBumiConvertToInteger <- function(mat) {
         mat <- round(as.matrix(mat))
         storage.mode(mat) <- "integer"
-        mat = mat[rowSums(mat) > 0,]
+        mat <- mat[rowSums(mat) > 0,]
         return(mat)
 }
 
 NBumiFitModel <- function(counts) {
 	vals <- hidden_calc_vals(counts);
-	mus <- (vals$tjs) %*% t(vals$tis/vals$total)
+#	mus <- (vals$tjs) %*% t(vals$tis/vals$total)
 	
 	convergence <- 0.001
-	max_size <- max(mus)^2;
+#	max_size <- max(mus)^2;
 	min_size <- 10^-10;
 #	size <- sapply(1:vals$ng,function(j) {
 #		 bg__fit_size_to_var(var(counts[j,]-mus[j,]), mus[j,], max_size=max_size, min_size=10^-10, convergence=0.001)
 #		})
+	my_rowvar <- vector(length=nrow(counts));
+	for (i in 1:nrow(counts)) {
+		mu_is <- vals$tjs[i]*vals$tis/vals$total
+		my_rowvar[i] <- var(counts[i,]-mu_is)
+	}
+		
+	size <- vals$tjs^2*(sum(vals$tis^2)/vals$total^2)/((vals$nc-1)*my_rowvar-vals$tjs) # for this to work with sparse matrices might need to implement in C
+	max_size <- 10*max(size);
+	size[size < 0] <- max_size;
+	size[size < min_size] <- min_size;
+#	size[size > max_size] <- max_size;
 
-	size = vals$tjs^2*(sum(vals$tis^2)/vals$total^2)/((vals$nc-1)*rowVars(counts-mus)-vals$tjs) # for this to work with sparse matrices might need to implement in C
-	size[size < 0] = max_size;
-	size[size < min_size] = min_size;
-	size[size > max_size] = max_size;
-
-	return(list(mus=mus, sizes=size, vals=vals))
+	return(list(var_obs=my_rowvar, sizes=size, vals=vals))
 }
 
 NBumiFitBasicModel <- function(counts) {
@@ -50,60 +56,74 @@ NBumiFitBasicModel <- function(counts) {
 	size <- mus^2/(v-mus)
 	max_size <- max(mus)^2;
 	size[errs] <- max_size;
-	mus_mat <- matrix(rep(mus, times=vals$nc), ncol=vals$nc, byrow=FALSE)
-	return(list(mus=mus_mat, sizes=size, vals=vals))
+	my_rowvar <- vector(length=nrow(counts));
+	for (i in 1:nrow(counts)) {
+		my_rowvar[i] <- var(counts[i,]-mus[i])
+	}
+	#mus_mat <- matrix(rep(mus, times=vals$nc), ncol=vals$nc, byrow=FALSE)
+	return(list(var_obs=my_rowvar, sizes=size, vals=vals))
 }
 
 NBumiCheckFit <- function(counts, fit, suppress.plot=FALSE) {
 	vals <- fit$vals;
 	size_mat <- matrix(rep(fit$sizes, times=vals$nc), ncol=vals$nc, byrow=FALSE)
-	vs <- fit$mus+fit$mus*fit$mus/size_mat
-	thing <- apply(counts-fit$mus, 1, var)
-#	plot(thing, rowMeans(vs), log="xy", xlab="Observed", ylab="Expected", main="Gene-specific Variance")
+#	vs <- vector(length=nrow(counts));
+	row_ps <- vector(length=nrow(counts))
+	col_ps <- rep(0, times=ncol(counts))
+	for (i in 1:nrow(counts)) {
+		mu_is <- vals$tjs[i]*vals$tis/vals$total
+		p_is <- (1+mu_is/fit$size[i])^(-fit$size[i]);
+		row_ps[i] <- sum(p_is);
+		col_ps <- col_ps+p_is;
+#		vs[i] <- mean(mu_is+mu_is*mu_is/fit$size[i])
+	}
+#	thing <- fit$var_obs
+#	plot(thing, vs, log="xy", xlab="Observed", ylab="Expected", main="Gene-specific Variance")
 #	abline(a=0, b=1, col="red")
 
-	exp_ps <- (1+fit$mus/size_mat)^(-size_mat)
+#	exp_ps <- (1+fit$mus/size_mat)^(-size_mat)
 	if (!suppress.plot) {
-		plot(vals$djs, rowSums(exp_ps), xlab="Observed", ylab="Fit", main="Gene-specific Dropouts")
+		plot(vals$djs, row_ps, xlab="Observed", ylab="Fit", main="Gene-specific Dropouts")
 		abline(a=0, b=1, col="red")
 
-		plot(vals$dis, colSums(exp_ps), xlab="Observed", ylab="Expected", main="Cell-specific Dropouts")
+		plot(vals$dis, col_ps, xlab="Observed", ylab="Expected", main="Cell-specific Dropouts")
 		abline(a=0, b=1, col="red")
 	}
 
-#	plot(vals$tjs/vals$nc, rowMeans(fit$mus), xlab="Observed", ylab="Fit", main="Gene Mean Expression")
-#	abline(a=0, b=1, col="red")
-
-#	plot(vals$tis, colSums(fit$mus), xlab="Observed", ylab="Fit", main="Cell Count Total")
-#	abline(a=0, b=1, col="red")
-	invisible(list(gene_error = sum((vals$djs-rowSums(exp_ps))^2), cell_error = sum((vals$dis-colSums(exp_ps))^2), exp_ps = exp_ps));
+	invisible(list(gene_error = sum((vals$djs-row_ps)^2), cell_error = sum((vals$dis-col_ps)^2), rowPs=row_ps, colPs=col_ps));
 }
 
 NBumiCheckFitFS <- function(counts, fit, suppress.plot=FALSE) {
 	vals <- fit$vals;
-	size_coeffs = NBumiFitDispVsMean(fit, suppress.plot=TRUE) 
-	smoothed_size = exp( size_coeffs[1] + size_coeffs[2]*log(vals$tjs/vals$nc) )
-	size_mat <- matrix(rep(smoothed_size, times=vals$nc), ncol=vals$nc, byrow=FALSE)
-	vs <- fit$mus+fit$mus*fit$mus/size_mat
-	thing <- apply(counts-fit$mus, 1, var)
-#	plot(thing, rowMeans(vs), log="xy", xlab="Observed", ylab="Expected", main="Gene-specific Variance")
+	size_coeffs <- NBumiFitDispVsMean(fit, suppress.plot=TRUE) 
+	smoothed_size <- exp( size_coeffs[1] + size_coeffs[2]*log(vals$tjs/vals$nc) )
+#	size_mat <- matrix(rep(smoothed_size, times=vals$nc), ncol=vals$nc, byrow=FALSE)
+
+# Matrix compatibility
+#	vs <- vector(length=nrow(counts));
+	row_ps <- vector(length=nrow(counts))
+	col_ps <- rep(0, times=ncol(counts))
+	for (i in 1:nrow(counts)) {
+		mu_is <- vals$tjs[i]*vals$tis/vals$total
+		p_is <- (1+mu_is/smoothed_size[i])^(-smoothed_size[i]);
+		row_ps[i] <- sum(p_is);
+		col_ps <- col_ps+p_is;
+#		vs[i] <- mean(mu_is+mu_is*mu_is/fit$size[i])
+	}
+#	thing <- fit$var_obs;
+#	plot(thing, vs, log="xy", xlab="Observed", ylab="Expected", main="Gene-specific Variance")
 #	abline(a=0, b=1, col="red")
 
-	exp_ps <- (1+fit$mus/size_mat)^(-size_mat)
+#	exp_ps <- (1+fit$mus/size_mat)^(-size_mat)
 	if (!suppress.plot) {
-		plot(vals$djs, rowSums(exp_ps), xlab="Observed", ylab="Fit", main="Gene-specific Dropouts")
+		plot(vals$djs, row_ps, xlab="Observed", ylab="Fit", main="Gene-specific Dropouts")
 		abline(a=0, b=1, col="red")
 
-		plot(vals$dis, colSums(exp_ps), xlab="Observed", ylab="Expected", main="Cell-specific Dropouts")
+		plot(vals$dis, col_ps, xlab="Observed", ylab="Expected", main="Cell-specific Dropouts")
 		abline(a=0, b=1, col="red")
 	}
 
-#	plot(vals$tjs/vals$nc, rowMeans(fit$mus), xlab="Observed", ylab="Fit", main="Gene Mean Expression")
-#	abline(a=0, b=1, col="red")
-
-#	plot(vals$tis, colSums(fit$mus), xlab="Observed", ylab="Fit", main="Cell Count Total")
-#	abline(a=0, b=1, col="red")
-	invisible(list(gene_error = sum((vals$djs-rowSums(exp_ps))^2), cell_error = sum((vals$dis-colSums(exp_ps))^2), exp_ps = exp_ps));
+	invisible(list(gene_error = sum((vals$djs-row_ps)^2), cell_error = sum((vals$dis-col_ps)^2), rowPs=row_ps, colPs=col_ps));
 }
 
 
@@ -118,12 +138,12 @@ NBumiCompareModels <- function(counts, size_factor=(colSums(counts)/median(colSu
 
 	plot( fit_adjust$vals$tjs/nc, fit_adjust$vals$djs/nc, col="black", pch=16, xlab="Expression", log="x", ylab= "Dropout Rate", cex=0.75)
 #	points( fit_adjust$vals$tjs/nc, fit_basic$vals$djs/nc, col="black", pch=16, cex=0.75)
-	points( fit_adjust$vals$tjs/nc, rowSums(check_adjust$exp_ps)/nc, col="goldenrod1", pch=16, cex=0.5 )
-	points( fit_adjust$vals$tjs/nc, rowSums(check_basic$exp_ps)/nc, col="purple", pch=16, cex=0.5 )
+	points( fit_adjust$vals$tjs/nc, check_adjust$rowPs/nc, col="goldenrod1", pch=16, cex=0.5 )
+	points( fit_adjust$vals$tjs/nc, check_basic$rowPs/nc, col="purple", pch=16, cex=0.5 )
 
-	err_adj <- sum(abs(rowSums(check_adjust$exp_ps)/nc-fit_adjust$vals$djs/nc))
+	err_adj <- sum(abs(check_adjust$rowPs/nc-fit_adjust$vals$djs/nc))
 #	err_bas <- sum(abs(rowSums(check_basic$exp_ps)/nc-fit_basic$vals$djs/nc))
-	err_bas <- sum(abs(rowSums(check_basic$exp_ps)/nc-fit_adjust$vals$djs/nc))
+	err_bas <- sum(abs(check_basic$rowPs/nc-fit_adjust$vals$djs/nc))
 	legend("bottomleft", paste(c("Depth-Adjusted\nError:", "Basic\nError:"), round(c(err_adj, err_bas)), c("\n","\n")), col=c("goldenrod1","purple"), pch=16, bty="n", cex=0.75)
 	out <- c(err_adj, err_bas); names(out) <- c("Depth-Adjusted", "Basic");
 	return(out)
@@ -251,12 +271,22 @@ bg__nbumiFeatureSelectionDropouts <- function(fit) {
 	# Gene-specific variance, mean
 	vals <- fit$vals;
 	size_mat <- matrix(rep(fit$sizes, times=vals$nc), ncol=vals$nc, byrow=F)
-	Exp_p <- (1+fit$mus/size_mat)^(-size_mat)
-	Exp_p_var <- Exp_p*(1-Exp_p)
 
-	droprate_exp <- rowSums(Exp_p)/vals$nc
+	droprate_exp <- vector(length=nrow(counts))
+	droprate_exp_err <- vector(length=nrow(counts))
+	for (i in 1:nrow(counts)) {
+		mu_is <- vals$tjs[i]*vals$tis/vals$total
+		p_is <- (1+mu_is/vals$size[i])^(-vals$size[i]);
+		p_var_is <- p_is*(1-p_is);
+		droprate_exp[i] <- sum(p_is)/vals$nc;
+		droprate_exp_err[i] <- sqrt(sum(p_var_is)/(vals$nc^2));
+	}
+#	Exp_p <- (1+fit$mus/size_mat)^(-size_mat)
+#	Exp_p_var <- Exp_p*(1-Exp_p)
+
+#	droprate_exp <- rowSums(Exp_p)/vals$nc
 	droprate_exp[droprate_exp < 1/vals$nc] <- 1/vals$nc # Is this necessary?
-	droprate_exp_err <- sqrt(rowSums(Exp_p_var)/(vals$nc^2))
+#	droprate_exp_err <- sqrt(rowSums(Exp_p_var)/(vals$nc^2))
 	droprate_obs <- vals$djs/vals$nc
 	droprate_obs_err <- sqrt(droprate_obs*(1-droprate_obs)/vals$nc)
 
@@ -275,13 +305,22 @@ NBumiFeatureSelectionCombinedDrop <- function(fit) {
 	coeffs <- NBumiFitDispVsMean(fit, suppress.plot=TRUE);
 	exp_size <- exp( coeffs[1] + coeffs[2]*log(vals$tjs/vals$nc) )
 
-	size_mat <- matrix(rep(exp_size, times=vals$nc), ncol=vals$nc, byrow=F)
-	Exp_p <- (1+fit$mus/size_mat)^(-size_mat)
-	Exp_p_var <- Exp_p*(1-Exp_p)
+	#size_mat <- matrix(rep(exp_size, times=vals$nc), ncol=vals$nc, byrow=F)
+	#Exp_p <- (1+fit$mus/size_mat)^(-size_mat)
+	#Exp_p_var <- Exp_p*(1-Exp_p)
+	droprate_exp <- vector(length=nrow(counts))
+	droprate_exp_err <- vector(length=nrow(counts))
+	for (i in 1:nrow(counts)) {
+		mu_is <- vals$tjs[i]*vals$tis/vals$total
+		p_is <- (1+mu_is/exp_size[i])^(-exp_size[i]);
+		p_var_is <- p_is*(1-p_is);
+		droprate_exp[i] <- sum(p_is)/vals$nc;
+		droprate_exp_err[i] <- sqrt(sum(p_var_is)/(vals$nc^2));
+	}
 
-	droprate_exp <- rowSums(Exp_p)/vals$nc
+	#droprate_exp <- rowSums(Exp_p)/vals$nc
 	droprate_exp[droprate_exp < 1/vals$nc] <- 1/vals$nc # Is this necessary?
-	droprate_exp_err <- sqrt(rowSums(Exp_p_var)/(vals$nc^2))
+	#droprate_exp_err <- sqrt(rowSums(Exp_p_var)/(vals$nc^2))
 	droprate_obs <- vals$djs/vals$nc
 	droprate_obs_err <- sqrt(droprate_obs*(1-droprate_obs)/vals$nc)
 
@@ -297,12 +336,21 @@ NBumiFeatureSelectionCombinedDrop <- function(fit) {
 PoissonUMIFeatureSelectionDropouts <- function(fit) {
 	# Poisson distribution
 	vals <- fit$vals
-	Exp_p <- exp(-fit$mus)
-	Exp_p_var <- Exp_p*(1-Exp_p)
+	#Exp_p <- exp(-fit$mus)
+	#Exp_p_var <- Exp_p*(1-Exp_p)
+	droprate_exp <- vector(length=nrow(counts))
+	droprate_exp_err <- vector(length=nrow(counts))
+	for (i in 1:nrow(counts)) {
+		mu_is <- vals$tjs[i]*vals$tis/vals$total
+		p_is <- exp(-mu_is);
+		p_var_is <- p_is*(1-p_is);
+		droprate_exp[i] <- sum(p_is)/vals$nc;
+		droprate_exp_err[i] <- sqrt(sum(p_var_is)/(vals$nc^2));
+	}
 
-	droprate_exp <- rowSums(Exp_p)/vals$nc
+	#droprate_exp <- rowSums(Exp_p)/vals$nc
 	droprate_exp[droprate_exp < 1/vals$nc] <- 1/vals$nc # Is this necessary?
-	droprate_exp_err <- sqrt(rowSums(Exp_p_var)/(vals$nc^2))
+	#droprate_exp_err <- sqrt(rowSums(Exp_p_var)/(vals$nc^2))
 	droprate_obs <- vals$djs/vals$nc
 	droprate_obs_err <- sqrt(droprate_obs*(1-droprate_obs)/vals$nc)
 
@@ -342,6 +390,8 @@ PoissonUMIFeatureSelectionDropouts <- function(fit) {
 #}
 #### Differential Expression #####
 
+# HERE HERE HERE - editing for Matrix sparse-matrix compatibility #
+
 bg__nbumiGroupDE <- function(counts, fit, groups) {
 	# Global mean-variance, gene-specific variance & mean
 	vals <- fit$vals;
@@ -363,9 +413,10 @@ bg__nbumiGroupDE <- function(counts, fit, groups) {
 		prob_test <- 0;
 		for (i in 1:vals$nc) {
 			group <- which(colnames(group_specific_factor) == groups[i])
-			p1 <- dnbinom(counts[j,i], mu=fit$mus[j,i], size=size_g[j], log=TRUE)
-			group_mu <- fit$mus[j,i]*group_specific_factor[j,group]
-			shifted_size <- hidden_shift_size(fit$mus[j,i],size_g[j],group_mu,coeffs);
+			mu_ji <- vals$tjs[j]*vals$tis[i]/vals$total
+			p1 <- dnbinom(counts[j,i], mu=mu_ji, size=size_g[j], log=TRUE)
+			group_mu <- mu_ji*group_specific_factor[j,group]
+			shifted_size <- hidden_shift_size(mu_ji,size_g[j],group_mu,coeffs);
 			p2 <- dnbinom(counts[j,i], mu=group_mu, size=shifted_size, log=TRUE)
 			# Collect ps across i
 			prob_null <- prob_null+p1
@@ -417,8 +468,36 @@ broken__nbumiCGroupDE <- function(counts, fit, groups) {
 }
 
 NBumiImputeZeros <- function(counts, fit) {
-	low <- counts < fit$mus
-#	counts[counts==0] = fit$mus[counts==0];
-	counts[low] = counts[low]+fit$mus[low];
+	vals <- fit$vals
+	for (i in 1:nrow(counts)) {
+		mu_is <- vals$tjs[i]*vals$tis/vals$total
+		low <- counts[i,] < mu_is
+		counts[i,low] <- counts[i,low]+mu_is[low]
+	}
 	return(counts);
 }
+
+NBumiImputeNorm <- function(counts, fit, total_counts_per_cell=NULL) {
+	# find p-value for current fit NB Umi
+	# adjust parameters of the NB
+	# determine value of equivalent p-value under new NB
+	vals <- fit$vals;
+	norm <- counts;
+	if (is.null(total_counts_per_cell)) {
+		normed_ti <- total_counts_per_cell;
+	} else {
+		normed_ti <- median(vals$tis);
+	}
+	normed_mus <- vals$tjs/vals$total;
+	normed_size <- fit$size;
+	for (i in 1:nrow(counts)) {
+		mu_is <- vals$tjs[i]*vals$tis/vals$total;
+		p_orig <- pnbinom(counts[i,], mu=mu_is, size=fit$sizes[i]);
+		normed <- qnbinom(p_orig, mu=normed_mus[i]*normed_ti, size=normed_size[i]);
+		norm[i,] <- normed;
+	}
+	return(norm);
+}
+
+## Cell-type DANB ##
+# fit variances with contribution from global mean~var relationship for small clusters. 
